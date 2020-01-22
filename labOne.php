867ebd6fe4628f1714995f1cf1fd9f58d6d6850d
Nitish Kafle <47726833+kaflenitish@users.noreply.github.com>
2020-01-22 13:18:15 -0500

    Add files via upload

---
+++ labOne.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<?php	#starts the php command
+
+/*
+Lab 1
+Name: Nitish Kafle
+Course: CSC306
+*/
+
+echo "My name is Nitish Kafle and I live in Maryland.<br>";
+echo "I am a Computer Science major and a Junior.<br>";
+echo "I drive a Chevy Cruze and my dream car is a Koenigsegg Agera.<br>";
+echo "My dream home would be somewhere in Bali.<br>";
+echo "My dream job is a Software Engineer.<br>";
+
+#echo prints the input given by the user! works similar to print statement in python
+
+echo "My hometown is Lumbini, birthplace of Buddha.<br>";
+echo "I have deep interest in AI and Data Science.<br>";
+echo "I build my first website in Grade 8 using wordpress.<br>";
+echo "I drink coffee alot.<br>";
+echo "I also want to work on Open Source Projects.";
+
+#ends php command with ? >
+?>		
+</body>
+<html>
